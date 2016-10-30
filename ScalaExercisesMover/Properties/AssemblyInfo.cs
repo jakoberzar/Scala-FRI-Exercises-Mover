4ab8fe9a4dfa60edcb507ae5bf0727d103e78165
Jakob Erzar <jakob.erzar@gmail.com>
2016-10-30 02:47:18 +0100

    Updated executable info

--- ScalaExercisesMover/Properties/AssemblyInfo.cs
+++ ScalaExercisesMover/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScalaExercisesMover")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FRI Scala Exercises Mover")]
+[assembly: AssemblyDescription("A small program to help peer reviewing the code.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScalaExercisesMover")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Jakob Erzar 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
